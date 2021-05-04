b6b88e8efedd56a0b637cfbd4c9534d027d97240
Leo Rondeau <christopherleorondeau@gmail.com>
2021-05-04 18:25:13 -0500

    had naming error

--- areyougoingapi/models/usersevents.py
+++ areyougoingapi/models/usersevents.py
@@ -3,5 +3,5 @@ from django.db import models
 class UsersEvents(models.Model):
 
     goer = models.ForeignKey("Goer", on_delete=models.CASCADE)
-    event = models.ForeignKey("Goer", on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
     
