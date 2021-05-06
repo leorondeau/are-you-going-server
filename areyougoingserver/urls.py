1141b1bd5c9b05c469aeac5e353421e237a74145
Leo Rondeau <christopherleorondeau@gmail.com>
2021-05-05 22:00:02 -0500

    added router path and url

--- areyougoingserver/urls.py
+++ areyougoingserver/urls.py
@@ -1,8 +1,16 @@
+from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
 from areyougoingapi.views import register_user, login_user
+from areyougoingapi.views import Events
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'events', Events, 'event')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
