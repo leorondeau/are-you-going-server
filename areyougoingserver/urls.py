3ca2136c7fd26a92353916a3adc90b7e06711ce0
Leo Rondeau <christopherleorondeau@gmail.com>
2021-05-10 22:02:52 -0500

    added profile router

--- areyougoingserver/urls.py
+++ areyougoingserver/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import include
 from django.urls import path
 from areyougoingapi.views import register_user, login_user
 from areyougoingapi.views import Events
+from areyougoingapi.views import Profile
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'events', Events, 'event')
+router.register(r'profile', Profile, 'profile')
 
 
 urlpatterns = [
