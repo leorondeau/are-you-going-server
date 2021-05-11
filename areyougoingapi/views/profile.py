7ad91f2b82714e7b844b8c54992d32d87e990c90
Leo Rondeau <christopherleorondeau@gmail.com>
2021-05-10 21:52:54 -0500

    profile viewset

--- areyougoingapi/views/profile.py
+++ areyougoingapi/views/profile.py
@@ -13,5 +13,33 @@ class Profile(ViewSet):
     def list(self, request):
 
         goer = Goer.objects.get(user=request.auth.user)
-        events = Event.objects.filter(goer=goer)
+        events = Event.objects.filter(goer =  goer)
         
+        events = EventSerializer( events, many=True, context={'request': request})
+        goer = GoerSerializer( goer, many = False, context={'request': request})
+
+        profile = {}
+        profile["goer"] = goer.data
+        profile["events"] = events.data
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username']
+
+class GoerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
+    class Meta:
+        model = Goer
+        fields = ['user']
+
+class EventSerializer(serializers.ModelSerializer):
+    goer = GoerSerializer(many=False)
+
+    class Meta:
+        model = Event
+        fields = ['id', 'goer' , 'name', 'location', 'startDate', 'details']
+
+        
+    
