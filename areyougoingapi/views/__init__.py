9683706f5c8c57b99dec4c1636877256318d487f
Leo Rondeau <christopherleorondeau@gmail.com>
2021-05-22 20:37:59 -0500

    added del_user to package

--- areyougoingapi/views/__init__.py
+++ areyougoingapi/views/__init__.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from .auth import login_user
 from .auth import register_user
+from .auth import del_user
 from .event import Events
 from .profile import Profile
