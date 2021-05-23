083ca05799b3873d0ea3c44b1c7f50a569eda995
Leo Rondeau <christopherleorondeau@gmail.com>
2021-05-22 20:38:36 -0500

    added router path for del

--- areyougoingserver/urls.py
+++ areyougoingserver/urls.py
@@ -2,6 +2,7 @@ from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
 from areyougoingapi.views import register_user, login_user
+from areyougoingapi.views import del_user
 from areyougoingapi.views import Events
 from areyougoingapi.views import Profile
 
@@ -11,10 +12,13 @@ router.register(r'events', Events, 'event')
 router.register(r'profile', Profile, 'profile')
 
 
+
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
+    path('del_user', del_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    
 ]
 
